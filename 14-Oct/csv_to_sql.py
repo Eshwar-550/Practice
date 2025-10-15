cf974a34d517ae09b91ee0b81c643ebcd510c045
Eshwar Thudimilla <108423114+Eshwar-550@users.noreply.github.com>
2025-10-15 17:00:52 +0530

    Create csv_to_sql.py

---
+++ 14-Oct/csv_to_sql.py
@@ -0,0 +1,23 @@
+# Have 10 to 15 lines of data in csv file.
+# Insert these records into employee table.
+# employee_data.csv(eg:
+# 101, Alice, 60000, 1
+# 102, Bob, 75000, 2...)
+
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user="root", password="Assassincreed@3", database="practice")
+cur = mydb.cursor()
+
+with open("employees_data.csv",'r') as file:
+    rows = file.readlines()
+    for i in rows:
+        lines = i.strip().split(",")
+
+        query = "INSERT INTO employees VALUES (%s,%s,%s,%s);"
+        cur.execute(query,lines)
+
+mydb.commit()
+cur.close()
+
+print("Rows inserted Successfully")
